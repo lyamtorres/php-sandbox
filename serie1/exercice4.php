ec6a583eb62ce4eaf60d1bdcc0ed990771bef65f
lyamtorres <lyamtorres@hotmail.com>
2022-01-15 11:21:26 +0100

    Finish 4th exercise of the first serie

--- serie1/exercice4.php
+++ serie1/exercice4.php
@@ -1,51 +1,51 @@
 <?php
-
-    $tab_dupont = ["prénom" => "PAUL","profession" => "ministre","age" => 50];
-    $tab_durand = ["prénom" => "ROBERT","profession" => "agriculteur","age" => 45];
-    $array = ["Dupont" => $tab_dupont, "Durant" => $tab_durand];
-
-    echo <<< _END
-        <table>
-            <tr>
-                <th>Clé</th>
-                <th>Valeur</th>
-            </tr>
-    _END;
-    foreach($array as $name => $subArray) {
-        echo <<< _END
-            <tr>
-                <td>$name</td>
-                <td>
-                    <table>
-                    <tr>
-                        <th>Clé</th>
-                        <th>Valeur</th>
-                    </tr>
-        _END;
-        foreach($subArray as $key => $value) {
-            echo <<< _END
-                        <tr>
-                            <td>$key</td>
-                            <td>$value</td>
-                        </tr>
-            _END;
-        }
-        echo <<< _END
-                    </table>
-                </td>
-            </tr>
-        _END;
-    }
-    echo "</table>";
-
+	$tab_dupont = ["prénom" => "PAUL","profession" => "ministre","age" => 50];
+	$tab_durand = ["prénom" => "ROBERT","profession" => "agriculteur","age" => 45];
+	$tab = ["Dupont" => $tab_dupont, "Durant" => $tab_durand];
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col"> Clé </th>
+                <th scope="col"> Valeur </th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            foreach ($tab as $nom => $tableau) {
+                echo '<tr>';
+                echo '<td> ' . $nom . '</td>';
+                echo '<td>';
+                echo '<table class="table">';
+                echo '<thead>';
+                echo '<tr>';
+                echo '<th scope="col"> Clé </th>';
+                echo '<th scope="col"> Valeur </th>';
+                echo '</tr>';
+                echo '</thead>';
+                echo '<tbody>';
+                
+                foreach ($tab[$nom] as $key => $value) {
+                    echo '<tr>';
+                    echo '<td style="width:50%">' . $key .'</td>';
+                    echo '<td style="width:50%">' . $value . '</td>';
+                    echo '</tr>';
+                }
+                
+                echo '</tbody>';
+                echo '</table>';
+                echo '</td>';
+                echo '</tr>';
+            }
+        ?>
+        </tbody>	
+    </table>
 </body>
 </html>
